a85602b0b4fc8beea2abbbc5d01229036af0bf4a
Felipe <felipe.leventhal@gmail.com>
2023-04-26 09:09:18 -0300

    Preenche frota

--- main.py
+++ main.py
@@ -12,3 +12,14 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
             lista.append([linha,coluna+n])
             n += 1
     return lista
+
+# Preenche a frota no tabuleiro
+
+def preenche_frota(frota,navio,linha,coluna,orientacao,tamanho):
+    posicao = define_posicoes(linha,coluna,orientacao,tamanho)
+    for k, lista in frota.items():
+        if k == navio:
+            lista.append(posicao)
+            return frota
+    frota[navio] = [posicao]
+    return frota
